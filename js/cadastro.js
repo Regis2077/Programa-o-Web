677578194406d0c40e3f16b9338a69eccab9cfd0
regis <gbrregispf@gmail.com>
2022-10-16 17:00:17 -0300

    fix route bug

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,8 +4,9 @@ const getEmail = document.querySelector('.email')
 const getPassword = document.querySelector('.senha')
 const submitRegister =  document.querySelector('.submit')
 
+const arr = [] //array vazio para onde vão ser colocados os objetos 
+
 //função para armazenar os dados do usuário e colocar no array
-console.log(arr)
 function registeredLogin(){
   const data ={
     email: getEmail.value,
@@ -17,17 +18,16 @@ function registeredLogin(){
   console.log(arr)
 }
 
-const arr = [] //array vazio para onde vão ser colocados os objetos 
 
 
-submitRegister.addEventListener("click", function(e){
+submitRegister.addEventListener("click", function (e){
   e.preventDefault() //quando aperta o botão de submit é padrão que a página recarregue, essa função previne esse comportamento
   
   console.clear() //para limpar o console e não ficar acumulando  
 
   registeredLogin()
 
-  window.location = './login.html'
+  if(arr.length > 2) window.location = "./login.html"
 }) 
 /* essa função captura o botão de submit e o 'addEventListener' fica escutando o click do botão, assim que for 
 cliclado ele executa tudo que tem dentro. No caso aí tem a função que fizemos em cima, sempre que clicado ele 
